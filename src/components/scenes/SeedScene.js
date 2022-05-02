e90aa2defe34a75d5cf8fd9e9644cf1a5a9aab53
Grant Chen <gvchen@princeton.edu>
2022-05-02 16:47:34 -0400

    github pages test

--- src/components/scenes/SeedScene.js
+++ src/components/scenes/SeedScene.js
@@ -17,7 +17,7 @@ class SeedScene extends Scene {
 
         // Init state
         this.state = {
-            gui: new Dat.GUI(), // Create GUI for scene
+            /*gui: new Dat.GUI(), // Create GUI for scene*/
             rotationSpeed: 1,
             updateList: [],
             updateEnemyList: [],
